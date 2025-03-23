fd84deb738a6a36da98bf23071b67b3ce4a12bd6
Ibrahim Gazi <coder.ibrahimgazi@gmail.com>
2025-03-23 21:39:13 +0600

    Complete Hero Section

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -2,18 +2,30 @@ import React from 'react';
 
 const Hero = () => {
     return (
-        <div className='grid grid-cols-2 gap-4 mt-4'>
+        <div className='grid grid-cols-2 gap-4 mt-16'>
             {/* Hero Left Side */}
             <div className="">
-                <div className='flex gap-6 bg-rgba-white-10'>
-                    <h4 className='text-white '>New</h4>
-                    <p className='text-white'>New roadmap added for Phase 4 - 2025</p>
+                <div className='flex gap-6 bg-[#2C3A47] w-fit align-center rounded-full p-2 pr-4 pl-4 backdrop-sepia-0'>
+                    <h4 className='text-white badge'>New</h4>
+                    <p className='text-white text-sm'>New roadmap added for Phase 4 - 2025</p>
+                </div>
+
+                {/* Banner Title */}
+                <div>
+                    <h1 className='mt-10 leading-[70px] uppercase text-[70px] font-bold text-white banner-title'>
+                        Unified Web
+                        <br />
+                        3.0 TRADE
+                    </h1>
+                    <p className='text-white mt-10'>Buy tokens now and reap the benefits of the blockchain revolution. Benefit and influence the development of a project
+                    </p>
+                    <button className='mt-10 rounded-full text-white wallet-btn cursor-pointer'>REGISTER</button>
                 </div>
             </div>
 
             {/* Hero Left Side */}
-            <div className="">
-
+            <div className="w-[400px] justify-self-end card">
+                
             </div>
         </div>
     );
